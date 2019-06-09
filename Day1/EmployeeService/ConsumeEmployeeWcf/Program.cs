6058c86811173e0c8beed5b94f247e75fb69515c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-09 23:55:11 +0530

    Added the client for WCF Service.

---
+++ Day1/EmployeeService/ConsumeEmployeeWcf/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsumeEmployeeWcf.ServiceReference1;
+using System.Data;
+using static System.Console;
+
+namespace ConsumeEmployeeWcf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Service1Client service1Client = new Service1Client();
+            var employees = service1Client.GetAllEmployees();
+
+            foreach(DataRow dataRow in employees.EmployeesTable.Rows)
+            {
+                WriteLine($"Name: {dataRow["Name"]}, Gender: {dataRow["Gender"]}");
+            }
+
+            WriteLine("\n\nPress any key ....");
+            ReadKey();
+
+        }
+    }
+}
