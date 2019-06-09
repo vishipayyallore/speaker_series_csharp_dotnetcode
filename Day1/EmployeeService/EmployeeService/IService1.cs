39dea21817bbe77e77a0b3a9e8d903475240f2f4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-09 23:45:08 +0530

    Simple WCF Service

---
+++ Day1/EmployeeService/EmployeeService/IService1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        EmployeesData GetAllEmployees();
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class EmployeesData
+    {
+
+        public EmployeesData()
+        {
+            EmployeesTable = new DataTable("EmployeesData");
+        }
+
+        [DataMember]
+        public DataTable EmployeesTable { get; set; }
+
+    }
+
+}
---
+++ Day1/EmployeeService/EmployeeService/Service1.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public EmployeesData GetAllEmployees()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["EmployeesDb"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SELECT Id, Name, Gender, Salary FROM Employee"))
+                {
+                    using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                    {
+                        command.Connection = connection;
+                        dataAdapter.SelectCommand = command;
+                        using (DataTable dt = new DataTable())
+                        {
+                            EmployeesData customers = new EmployeesData();
+                            dataAdapter.Fill(customers.EmployeesTable);
+                            return customers;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
