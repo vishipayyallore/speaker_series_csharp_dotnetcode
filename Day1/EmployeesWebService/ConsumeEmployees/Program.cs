3097c5e10e1742610c2a8ba1d5cc2171fd7dbc41
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-09 23:24:32 +0530

    Added Client

---
+++ Day1/EmployeesWebService/ConsumeEmployees/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsumeEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceReference1.AbcEmployeesSoapClient abcEmployeesSoapClient = new ServiceReference1.AbcEmployeesSoapClient("AbcEmployeesSoap");
+            var employees = abcEmployeesSoapClient.GetAllEmployees();
+            foreach (var employee in employees)
+            {
+                WriteLine($"Name: {employee.Name} Salary: {employee.Salary}");
+            }
+
+            WriteLine("\n\nPress any key ...");
+            ReadKey();
+        }
+    }
+}
