7e086d058ee9cffb00e5a4d609f64b02a40fcb80
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-10 08:25:04 +0530

    Added another method

--- Day1/FirstWebApi/FirstWebApi/Controllers/EmployeesController.cs
+++ Day1/FirstWebApi/FirstWebApi/Controllers/EmployeesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,25 +13,32 @@ namespace FirstWebApi.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
-        readonly IConfiguration _configuration;
+        readonly EmployeesDbContext _employeesDbContext;
 
         public EmployeesController(IConfiguration configuration)
         {
-            _configuration = configuration;
+            var _configuration = configuration;
+
+            var optionsBuilder = new DbContextOptionsBuilder<EmployeesDbContext>();
+
+            var connectionString = _configuration["ConnectionStrings:EmployeesDBConnectionString"];
+            optionsBuilder.UseSqlServer(connectionString);
+
+            _employeesDbContext = new EmployeesDbContext(optionsBuilder.Options);
         }
 
 
         [HttpGet]
         public ActionResult<IEnumerable<Employee>> Get()
         {
-            var optionsBuilder = new DbContextOptionsBuilder<EmployeesDbContext>();
+            return _employeesDbContext.Employee.ToList();
+        }
 
-            var connectionString = _configuration["ConnectionStrings:EmployeesDBConnectionString"];
-            optionsBuilder.UseSqlServer(connectionString);
 
-            EmployeesDbContext employeesDbContext = new EmployeesDbContext(optionsBuilder.Options);
-
-            return employeesDbContext.Employee.ToList();
+        [HttpGet("{id}")]
+        public Employee GetEmployee(Guid id)
+        {
+            return _employeesDbContext.Employee.Where(record => record.Id == id).FirstOrDefault();
         }
 
     }
