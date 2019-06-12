4be8c914d0e50a452ed405eb73b85fec7fe3467e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-12 14:21:17 +0530

    Removed Unused usings

--- Day2/ADCEmployeesAPI/ADCEmployeesAPI/Controllers/EmployeesController.cs
+++ Day2/ADCEmployeesAPI/ADCEmployeesAPI/Controllers/EmployeesController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ADCEmployeesAPI.Entities;
-using Microsoft.AspNetCore.Http;
+﻿using ADCEmployeesAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ADCEmployeesAPI.Controllers
 {
