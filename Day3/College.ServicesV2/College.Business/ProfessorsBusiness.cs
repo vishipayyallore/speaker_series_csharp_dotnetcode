e4881a4274ae1becf1f76428930ed79a98b7c703
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-17 12:12:36 +0530

    Initial Version

---
+++ Day3/College.ServicesV2/College.Business/ProfessorsBusiness.cs
@@ -0,0 +1,47 @@
+﻿using College.Data;
+using College.Data.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace College.Business
+{
+
+    public class ProfessorsBusiness
+    {
+        readonly ProfessorsData _professorsData;
+
+        public ProfessorsBusiness(IConfiguration configuration)
+        {
+            _professorsData = new ProfessorsData(configuration);
+        }
+
+        public IEnumerable<Professor> GetProfessors()
+        {
+            return _professorsData.GetProfessors();
+        }
+
+        public Professor GetProfessorById(Guid id)
+        {
+            return _professorsData.GetProfessorById(id);
+        }
+
+
+        public Professor AddProfessor(Professor professor)
+        {
+            return _professorsData.AddProfessor(professor);
+        }
+
+        public Professor UpdateProfessor(Professor professor)
+        {
+            return _professorsData.UpdateProfessor(professor);
+        }
+
+        public bool DeleteProfessorById(Guid id)
+        {
+            return _professorsData.DeleteProfessorById(id);
+        }
+
+    }
+
+}
---
+++ Day3/College.ServicesV2/College.Data/Persistence/CollegeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using College.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace College.Data.Persistence
+{
+
+    public class CollegeDbContext : DbContext
+    {
+
+        public CollegeDbContext(DbContextOptions<CollegeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Professor> Professors { get; set; }
+    }
+
+}
