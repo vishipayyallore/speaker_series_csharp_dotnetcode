e5d584a83c3c64282162f1436d5d98105b94112f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-17 23:15:26 +0530

    Initial Version for Dependency Injection

---
+++ Day4/College.ServicesV3/College.Business/ProfessorsBusiness.cs
@@ -0,0 +1,48 @@
+﻿using College.Data;
+using College.Data.Entities;
+using College.Data.Persistence;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace College.Business
+{
+
+    public class ProfessorsBusiness
+    {
+        readonly ProfessorsData _professorsData;
+
+        public ProfessorsBusiness(CollegeDbContext collegeDbContext)
+        {
+            _professorsData = new ProfessorsData(collegeDbContext);
+        }
+
+        public IEnumerable<Professor> GetProfessors()
+        {
+            return _professorsData.GetProfessors();
+        }
+
+        public Professor GetProfessorById(Guid id)
+        {
+            return _professorsData.GetProfessorById(id);
+        }
+
+
+        public Professor AddProfessor(Professor professor)
+        {
+            return _professorsData.AddProfessor(professor);
+        }
+
+        public Professor UpdateProfessor(Professor professor)
+        {
+            return _professorsData.UpdateProfessor(professor);
+        }
+
+        public bool DeleteProfessorById(Guid id)
+        {
+            return _professorsData.DeleteProfessorById(id);
+        }
+
+    }
+
+}
---
+++ Day4/College.ServicesV3/College.Comman/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace College.Comman
+{
+    public static class Constants
+    {
+        public static string ConnectionString { get; set; } = "ConnectionStrings:CollegeDBConnectionString";
+    }
+
+}
