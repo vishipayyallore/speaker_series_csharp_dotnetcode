aca7b170ee1ef3f40445477b2071b68c1b3ac5b6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-18 15:17:34 +0530

    Inital Version

---
+++ Day4/College.ServicesV/College.Business/ProfessorsBusiness.cs
@@ -0,0 +1,46 @@
+﻿using College.Comman.Entities;
+using College.Comman.Interface;
+using System;
+using System.Collections.Generic;
+
+namespace College.Business
+{
+
+    public class ProfessorsBusiness : IProfessorsBusiness
+    {
+        readonly IProfessorsData _professorsData;
+
+        public ProfessorsBusiness(IProfessorsData professorsData)
+        {
+            _professorsData = professorsData;
+        }
+
+        public IEnumerable<Professor> GetProfessors()
+        {
+            return _professorsData.GetProfessors();
+        }
+
+        public Professor GetProfessorById(Guid id)
+        {
+            return _professorsData.GetProfessorById(id);
+        }
+
+
+        public Professor AddProfessor(Professor professor)
+        {
+            return _professorsData.AddProfessor(professor);
+        }
+
+        public Professor UpdateProfessor(Professor professor)
+        {
+            return _professorsData.UpdateProfessor(professor);
+        }
+
+        public bool DeleteProfessorById(Guid id)
+        {
+            return _professorsData.DeleteProfessorById(id);
+        }
+
+    }
+
+}
---
+++ Day4/College.ServicesV/College.Comman/Entities/Professor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace College.Comman.Entities
+{
+    public class Professor
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Doj { get; set; }
+
+        public string Teaches { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public bool IsPhd { get; set; }
+
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+
+}
