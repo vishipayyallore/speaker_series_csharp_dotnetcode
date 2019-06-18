ebced1c66c9f8c46e8459c66014ed245e56a1985
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-19 00:31:32 +0530

    Simple Web Api from Empty Template

---
+++ Day5/ConfigureFrameworkServices/ConfigureFrameworkServices/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigureFrameworkServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<List<object>> Get()
+        {
+            var students = new List<object>
+            {
+                new { Id = 101, Name = "Shiva Sai"},
+                new { Id = 102, Name = "Mathew Philips"},
+                new { Id = 103, Name = "Mohd Hafeez"}
+            };
+
+            return students;
+        }
+
+    }
+}
