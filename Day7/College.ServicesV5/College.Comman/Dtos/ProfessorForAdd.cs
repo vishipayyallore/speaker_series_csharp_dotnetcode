13843581061ed42c6d9b1a7db94d11454ed84eb9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-28 21:34:33 +0530

    Populated the Professor class

--- Day7/College.ServicesV5/College.Comman/Dtos/ProfessorForAdd.cs
+++ Day7/College.ServicesV5/College.Comman/Dtos/ProfessorForAdd.cs
@@ -1,6 +1,19 @@
-﻿namespace College.Common.Dtos
+﻿using System;
+
+namespace College.Common.Dtos
 {
-    class ProfessorForAdd
+
+    public class ProfessorForAdd
     {
+        public string Name { get; set; }
+
+        public DateTime Doj { get; set; }
+
+        public string Teaches { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public bool IsPhd { get; set; }
     }
+
 }
--- Day7/College.ServicesV5/College.Comman/Dtos/ProfessorForViewDto.cs
+++ Day7/College.ServicesV5/College.Comman/Dtos/ProfessorForViewDto.cs
@@ -1,6 +1,24 @@
-﻿namespace College.Common.Dtos
+﻿using College.Common.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace College.Common.Dtos
 {
-    class ProfessorForViewDto
+    public class ProfessorForViewDto
     {
+        public Guid ProfessorId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Doj { get; set; }
+
+        public string Teaches { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public bool IsPhd { get; set; }
+
+        public ICollection<Student> Students { get; set; } = new List<Student>();
     }
+
 }
