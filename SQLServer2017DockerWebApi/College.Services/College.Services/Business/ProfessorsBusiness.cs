17102b54a22b3640e4f0b66fb72e45da19c913ec
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-06-29 12:03:24 +0530

    Initial Version

---
+++ SQLServer2017DockerWebApi/College.Services/College.Services/Business/ProfessorsBusiness.cs
@@ -0,0 +1,46 @@
+﻿using College.Services.Data;
+using College.Services.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace College.Services.Business
+{
+    public class ProfessorsBusiness
+    {
+        readonly ProfessorsData _professorsData;
+
+        public ProfessorsBusiness(IConfiguration configuration)
+        {
+            _professorsData = new ProfessorsData(configuration);
+        }
+
+        public IEnumerable<Professor> GetProfessors()
+        {
+            return _professorsData.GetProfessors();
+        }
+
+        public Professor GetProfessorById(Guid id)
+        {
+            return _professorsData.GetProfessorById(id);
+        }
+
+
+        public Professor AddProfessor(Professor professor)
+        {
+            return _professorsData.AddProfessor(professor);
+        }
+
+        public Professor UpdateProfessor(Professor professor)
+        {
+            return _professorsData.UpdateProfessor(professor);
+        }
+
+        public bool DeleteProfessorById(Guid id)
+        {
+            return _professorsData.DeleteProfessorById(id);
+        }
+
+    }
+
+}
---
+++ SQLServer2017DockerWebApi/College.Services/College.Services/Data/ProfessorsData.cs
@@ -0,0 +1,89 @@
+﻿using College.Services.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace College.Services.Data
+{
+
+    public class ProfessorsData
+    {
+        readonly CollegeDbContext _collegeDbContext;
+
+        public ProfessorsData(IConfiguration configuration)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CollegeDbContext>();
+
+            var connectionString = configuration["ConnectionStrings:CollegeDBConnectionString"];
+            optionsBuilder.UseSqlServer(connectionString);
+
+            _collegeDbContext = new CollegeDbContext(optionsBuilder.Options);
+        }
+
+        public IEnumerable<Professor> GetProfessors()
+        {
+            return _collegeDbContext.Professors.ToList();
+        }
+
+
+        public Professor GetProfessorById(Guid id)
+        {
+            if (!_collegeDbContext.Professors.Any(record => record.Id == id))
+            {
+                return null;
+            }
+
+            return _collegeDbContext.Professors.Where(record => record.Id == id).FirstOrDefault();
+        }
+
+        public Professor AddProfessor(Professor professor)
+        {
+            _collegeDbContext.Professors.Add(professor);
+
+            _collegeDbContext.SaveChanges();
+
+            return professor;
+        }
+
+
+        public Professor UpdateProfessor(Professor professor)
+        {
+            if (!_collegeDbContext.Professors.Any(record => record.Id == professor.Id))
+            {
+                return null;
+            }
+
+
+            var retrievedProfessor = _collegeDbContext.Professors.Where(record => record.Id == professor.Id).FirstOrDefault();
+
+            // Modifying the data
+            retrievedProfessor.Salary = professor.Salary;
+            retrievedProfessor.IsPhd = professor.IsPhd;
+
+            _collegeDbContext.SaveChanges();
+
+            return professor;
+        }
+
+
+        public bool DeleteProfessorById(Guid id)
+        {
+            if (!_collegeDbContext.Professors.Any(record => record.Id == id))
+            {
+                return false;
+            }
+
+            var retrievedProfessor = _collegeDbContext.Professors.Where(record => record.Id == id).FirstOrDefault();
+
+            _collegeDbContext.Professors.Remove(retrievedProfessor);
+
+            _collegeDbContext.SaveChanges();
+
+            return true;
+        }
+
+    }
+
+}
