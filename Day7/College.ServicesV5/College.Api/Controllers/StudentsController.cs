5a6aefbc7654f520a43ab98089fe49c3eb19f43e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-07-05 22:36:33 +0530

    Removed unused usings

--- Day7/College.ServicesV5/College.Api/Controllers/StudentsController.cs
+++ Day7/College.ServicesV5/College.Api/Controllers/StudentsController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace College.Api.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class StudentsController : ControllerBase
     {
     }
+
 }
