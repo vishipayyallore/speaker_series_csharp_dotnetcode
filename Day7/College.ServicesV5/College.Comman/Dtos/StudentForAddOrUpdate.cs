e54d263d6b039610eae02f6007c0cf0512da98eb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-07-17 20:00:47 +0530

    Dto class

---
+++ Day7/College.ServicesV5/College.Comman/Dtos/StudentForAddOrUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace College.Common.Dtos
+{
+
+    public class StudentForAddOrUpdate
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string RollNumber { get; set; }
+
+        [Required]
+        public Guid ProfessorId { get; set; }
+
+        [Required]
+        // [Range(typeof(bool), "false", "true", ErrorMessage = "The field Is Active must be checked.")]
+        public decimal Salary { get; set; }
+    }
+
+}
