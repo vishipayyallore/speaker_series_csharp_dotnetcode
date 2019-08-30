e6fc3e232dfd299ff92e96bc01879554bed17ba5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-08-30 23:16:18 +0530

    Hocked up the EF Core

--- CollegeMicroService/College.Services/Startup.cs
+++ CollegeMicroService/College.Services/Startup.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using College.Services.BAL;
+using College.Services.Common;
+using College.Services.Persistence;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,6 +23,15 @@ namespace College.Services
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Adding EF Core
+            var connectionString = Configuration[Constants.ConnectionString];
+            services.AddDbContext<CollegeDbContext>(o => o.UseSqlServer(connectionString));
+
+
+            // Application Services
+            services.AddScoped<ProfessorsBal>();
+            services.AddScoped<ProfessorsDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
