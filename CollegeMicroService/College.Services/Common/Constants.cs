33ed71798cdc86cfd339c8f5132727b28abd2036
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-08-30 23:12:13 +0530

    Initial Version

---
+++ CollegeMicroService/College.Services/Common/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace College.Services.Common
+{
+
+    public static class Constants
+    {
+        public static string ConnectionString { get; set; } = "ConnectionStrings:CollegeDBConnectionString";
+    }
+
+}
---
+++ CollegeMicroService/College.Services/Persistence/CollegeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using College.Services.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace College.Services.Persistence
+{
+
+    public class CollegeDbContext : DbContext
+    {
+
+        public CollegeDbContext(DbContextOptions<CollegeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Professor> Professors { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+    }
+
+}
